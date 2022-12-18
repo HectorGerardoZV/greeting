c77a90562fd47cd353f20c636f7e3144f0b4ecec
HectorGerardoZV <torrozapata@gmail.com>
2022-12-18 01:52:46 -0700

    Changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import {config} from "dotenv";
+config({path:".env"});
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port,()=>{
+    console.log(`Server running in ${port}`);
+    
+  });
 }
 bootstrap();
